14a635da48e5efaceaa48eaf198f3cecf7cfa030
Christoph Buelter <buelter.christoph@gmail.com>
2022-02-17 17:05:40 +0100

    Convert seconds to mm:ss string

--- yt2web/core/views.py
+++ yt2web/core/views.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.decorators import login_required
+import datetime
+
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from yt2web.core.models import Playlist
 
 
+def to_mm_ss_string(seconds: int) -> str:
+    return str(datetime.timedelta(seconds=seconds)).replace("0:", "", 1)
+
+
 @login_required
-def playlist(request, playlist_id):
+def playlist(request, playlist_id: int):
     songs = []
     playlist = get_object_or_404(Playlist, id=playlist_id)
     for video in playlist.videos.all().order_by("title"):
@@ -15,7 +21,7 @@ def playlist(request, playlist_id):
                 "album": "",
                 "artist": "",
                 "cover_art_url": video.thumbnail_file.url,
-                "duration": video.duration,
+                "duration": to_mm_ss_string(video.duration),
                 "name": video.title,
                 "url": video.content_file.url,
             }

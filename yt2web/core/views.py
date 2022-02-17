352b6ded7b0503e884d59f363a15576460ac65fd
Christoph Buelter <buelter.christoph@gmail.com>
2022-02-17 16:14:18 +0100

    Play around with an amplitudejs based player page

--- yt2web/core/views.py
+++ yt2web/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from yt2web.core.models import Playlist
+
+
+def playlists(request):
+    songs = []
+    for playlist in Playlist.objects.all().order_by("title"):
+        for video in playlist.videos.all().order_by("title"):
+            songs.append(
+                {
+                    "name": video.title,
+                    "artist": "",
+                    "album": "",
+                    "url": video.content_file.url,
+                    "cover_art_url": video.thumbnail_file.url,
+                }
+            )
+
+    return render(request, "core/playlists.html", {"songs": songs})
--- yt2web/urls.py
+++ yt2web/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from yt2web.core import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.playlists),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
